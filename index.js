f5aae43b6271c492c6669272497ec4ecc48839b1
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2024-03-14 20:26:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
 
-  app.use(express.static(path.join(__dirname, './frontend/build')));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get('*', (req, res) =>{
       res.sendFile(path.resolve(__dirname, "frontend" , "build" , "index.html"))
   })
