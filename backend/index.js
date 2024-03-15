d6c7768d88a5b485500026e3dd60cddc745df491
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2024-03-15 13:49:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -51,10 +51,12 @@ app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
 
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get('*', (req, res) =>{
-      res.sendFile(path.resolve(__dirname, "frontend" , "build" , "index.html"))
-  })
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 app.use(errorMiddleware)
 
