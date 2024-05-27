9f6674ea200ce6ec2373b9a05740db462fa357f0
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2024-05-27 11:42:33 +0530

    Update auth.js

--- backend/utills/auth.js
+++ backend/utills/auth.js
@@ -2,17 +2,31 @@ const jwt = require('jsonwebtoken');
 const ls = require('localstorage-slim')
 
 exports.isAuth = async (req, res, next) => {
-    const token = ls.get('token')
-    console.log(token)
-    if (token) {
-        let data =  jwt.verify(token, process.env.SECRET_KEY);
-        req.user = data.user
-        if(!req.user){
-            return res.status(401).send({message: 'Not authorized.'})
-        }
-                    
-        return next();
-    }
+    try {
+		const token = req.cookies.accessToken
 
-    return res.status(401).send({message: 'Not authorized'})
-}
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		if (!decoded) {
+			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+		}
+
+		const user = await User.findById(decoded.userId).select("-password");
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		req.user = user;
+
+		next();
+	} catch (error) {
+		console.log("Error in protectRoute middleware: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+    
